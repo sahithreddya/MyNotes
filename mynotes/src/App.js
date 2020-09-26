c2e14599c3f270013fda9d92cf0c3f63139cdba5
Sahith Reddy <sahithreddy.a@gmail.com>
2020-09-26 18:25:09 +0530

    First Commit

---
+++ mynotes/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import MyNotes from './main'
+
+function App() {
+  return (
+    <MyNotes></MyNotes>
+  );
+}
+
+export default App;
---
+++ mynotes/src/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function MyNotes() {
+  return (
+    <div className="App">
+        <h1>Welcome to MyNotes</h1>
+    </div>
+  );
+}
+
+export default MyNotes;
